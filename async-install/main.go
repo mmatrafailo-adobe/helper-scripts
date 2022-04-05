ecdcf29e3de235c026647b26b0356270f167c6c7
npuchko <npuchko@adobe.com>
2022-04-04 20:44:51 -0500

    fixed wdi, reworked async install

--- async-install/main.go
+++ async-install/main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"io/ioutil"
+	"log"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 	"sync"
 	"time"
 )
@@ -16,26 +23,109 @@ func sum(a int, b int, c chan int) {
 	close(c)
 }
 
+func runCommandWithChannel(command string, dir string, msg chan string) {
+	commandObject := exec.Command("bash", "-c", command)
+	commandObject.Dir = dir
+	out, err := commandObject.CombinedOutput()
+	msg <- string(out)
+	if err != nil {
+		msg <- "Failed to execute command " + command + " Error: " + err.Error()
+	}
+}
+
+func createFolderAndInitProject(dir string, projectName string, jiraTicketUrl string, msg chan string) {
+	os.Mkdir(dir, 0777)
+
+	runCommandWithChannel("warden env-init "+projectName+" magento2", dir, msg)
+	runCommandWithChannel("warden sign-certificate "+projectName+".test", dir, msg)
+
+	if jiraTicketUrl != "" {
+		f, err := os.OpenFile(dir+"/.env", os.O_APPEND|os.O_WRONLY, 0644)
+		defer f.Close()
+		if err == nil {
+			f.WriteString("JIRA_TICKET_URL=" + jiraTicketUrl)
+		} else {
+			msg <- "error with writing .env file: " + err.Error()
+		}
+	}
+
+	runCommandWithChannel("warden env up", dir, msg)
+
+}
+
+func makeIdeaFolder(dir string) {
+
+	ideaFolderPath := dir + "/.idea"
+	os.Mkdir(ideaFolderPath, 0777)
+	uuidWithHyphen := uuid.New()
+
+	dataSources := ""
+
+	ioutil.WriteFile(ideaFolderPath+"/dataSources.xml", []byte(data), 0664)
+}
+
+func getProjectNameFromJIRAURL(jiraUrl string) string {
+	// take last path part jira.com/browse/MDVA-1234?fwefewf=ewf => MDVA-1234
+	parts := strings.Split(jiraUrl, "/")
+	projectNamePart := parts[len(parts)-1]
+	if strings.Contains(projectNamePart, "?") {
+		parts = strings.Split(projectNamePart, "?")
+		projectNamePart = parts[0]
+	}
+
+	// remove all except letters and numbers
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+	projectNamePart = reg.ReplaceAllString(projectNamePart, "")
+
+	return projectNamePart
+}
+
 func main() {
 	start := time.Now()
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Enter JIRA url or folder name (skip if it is a current dir): ")
+	var jiraTicketUrl string
+	fmt.Scan(jiraTicketUrl)
+
+	var projectNameAndDir string
+	if jiraTicketUrl == "" {
+		projectNameAndDir = filepath.Base(currentDir)
+	} else if strings.Contains(projectNameAndDir, "http") || strings.Contains(projectNameAndDir, "/") {
+		projectNameAndDir = getProjectNameFromJIRAURL(jiraTicketUrl)
+	} else {
+		projectNameAndDir = getProjectNameFromJIRAURL(jiraTicketUrl)
+		jiraTicketUrl = ""
+	}
 
 	fmt.Print("Enter PROJECT ID: ")
 	var projectId string
 	fmt.Scan(&projectId)
 
-	fmt.Println("Project id is " + projectId)
+	if projectId != "" {
+		fmt.Println("Project id is " + projectId)
 
-	listEnvsString := runCommand(" magento-cloud environment:list --project " + projectId + " --pipe")
-	fmt.Println(listEnvsString)
+		listEnvsString := runCommand(" magento-cloud environment:list --project " + projectId + " --pipe")
+		fmt.Println(listEnvsString)
 
-	var envId string
-	fmt.Print("Enter env id: ")
-	fmt.Scan(&envId)
+		var envId string
+		fmt.Print("Enter env id: ")
+		fmt.Scan(&envId)
 
-	// ucxdbrjol65si
-	sshPath := runCommand("magento-cloud env:ssh --project " + projectId + " --environment " + envId + " --pipe")
+		// ucxdbrjol65si
+		sshPath := runCommand("magento-cloud env:ssh --project " + projectId + " --environment " + envId + " --pipe")
 
-	fmt.Println(sshPath)
+		fmt.Println(sshPath)
+	} else {
+		fmt.Println("Project ID is empty, Skipping downloading the dumps")
+	}
 
 	//c := make(chan int)
 	////wg.Add(1)
