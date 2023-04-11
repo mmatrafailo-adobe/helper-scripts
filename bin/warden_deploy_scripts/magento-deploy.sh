e08804e34c22f57c9bed8d2c76b941a6951693ca
npuchko <npuchko@adobe.com>
2023-04-11 14:50:59 -0700

    Added additional module disable

--- bin/warden_deploy_scripts/magento-deploy.sh
+++ bin/warden_deploy_scripts/magento-deploy.sh
@@ -2,4 +2,5 @@ php bin/magento deploy:mode:set developer
 php bin/magento a:c:i
 php bin/magento ca:f
 php bin/magento admin:user:create --admin-user admin --admin-password 123123q --admin-email admin@example.com --admin-firstname admf --admin-lastname adml
+php bin/magento mo:di Magento_AdminAdobeImsTwoFactorAuth
 php bin/magento mo:di Magento_TwoFactorAuth
